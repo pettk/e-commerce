bda26d76fee0405cb32588190d77d354be4d0b46
pettk <134705354+pettk@users.noreply.github.com>
2024-05-07 21:35:42 +0700

    auth route/register/login

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const userInstance = require('../models/user');
+const AuthService = require('../service/authService');
+const passport = require('passport');
+
+router.post('/register', async (req, res) => {
+    const data = req.body;
+    try {
+        const newUser = await AuthService.register(data);
+        res.status(201).send(newUser);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+router.post('/login', passport.authenticate("local"), async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const loginUser = await AuthService.login({ username, password });
+        res.status(200).send(loginUser);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+module.exports = router;
